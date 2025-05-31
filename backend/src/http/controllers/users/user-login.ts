0b5eb7e12882b8304e2a4afaabb9bbd49982bbda
Gui-dev <adrianon10@gmail.com>
2025-05-31 04:42:13 -0300

    feat: add user login routes

---
+++ backend/src/http/controllers/users/user-login.ts
@@ -0,0 +1,37 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import {
+  userLoginResponse,
+  userLoginSchema,
+} from '../../validations/users/user-login-schema'
+import { UserRepository } from '../../../repositories/user-repository'
+import { UserLoginUseCase } from '../../../use-cases/users/user-login'
+
+export const userLoginRoute = (app: FastifyInstance) => {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    'users/login',
+    {
+      schema: {
+        summary: 'Logar usuário',
+        tags: ['users'],
+        body: userLoginSchema,
+        response: {
+          200: userLoginResponse,
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userRepository = new UserRepository()
+      const userLoginUseCase = new UserLoginUseCase(userRepository)
+
+      const { accessToken, refreshToken } = await userLoginUseCase.execute({
+        email,
+        password,
+      })
+
+      return reply.status(200).send({ accessToken, refreshToken })
+    },
+  )
+}
