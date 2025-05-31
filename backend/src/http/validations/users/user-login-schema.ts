a3f5782b10578f840e4289306d4e9b2709101136
Gui-dev <adrianon10@gmail.com>
2025-05-31 04:41:43 -0300

    feat: add user login schema

---
+++ backend/src/http/validations/users/user-login-schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+export const userLoginResponse = z.object({
+  accessToken: z.string(),
+  refreshToken: z.string(),
+})
