887ca1a1d87c4864405548f674ce4579f2ffd2cf
Gui-dev <adrianon10@gmail.com>
2025-06-06 04:39:34 -0300

    feat: add create job schema

---
+++ backend/src/http/validations/jobs/create-job-schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const createJobSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  company: z.string(),
+  location: z.string(),
+  type: z.enum(['REMOTE', 'ONSITE', 'HYBRID']),
+  technologies: z.array(z.string()).min(1),
+})
+
+export const createJobResponseSchema = z.object({
+  jobId: z.string().cuid(),
+})
