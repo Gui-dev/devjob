f6683de1e346980eeb05467fc8a64f4e52ace1fb
Gui-dev <adrianon10@gmail.com>
2025-06-07 21:01:16 -0300

    feat: add list jobs with filter use case

---
+++ backend/src/use-cases/jobs/list-jobs-with-filter.ts
@@ -0,0 +1,33 @@
+import type { IJobRepositoryContract } from '@/contracts/job-repository-contract'
+import type { JobLevel, JobType } from '../../../prisma/generated/prisma'
+
+interface IListJobsFiltersProps {
+  type?: JobType
+  level?: JobLevel
+  technology?: string
+  location?: string
+}
+
+export class ListJobsWithFilters {
+  constructor(private jobsRepository: IJobRepositoryContract) {}
+
+  public async execute({
+    type,
+    level,
+    technology,
+    location,
+  }: IListJobsFiltersProps) {
+    const jobs = await this.jobsRepository.findManyWithFilters({
+      type,
+      level,
+      technology,
+      location,
+    })
+
+    if (!jobs || jobs.length === 0) {
+      throw new Error('Jobs not found')
+    }
+
+    return { jobs }
+  }
+}
