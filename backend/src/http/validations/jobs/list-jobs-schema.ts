b63a40ba8ce34bae41f7baa551b966be40d3e9d4
Gui-dev <adrianon10@gmail.com>
2025-06-07 21:00:45 -0300

    feat: add list jobs schema

---
+++ backend/src/http/validations/jobs/list-jobs-schema.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+
+export const listJobsSchema = z.object({
+  technology: z.string().optional(),
+  type: z.enum(['REMOTE', 'ONSITE', 'HYBRID']).optional(),
+  level: z.enum(['JUNIOR', 'PLENO', 'SENIOR']).optional(),
+  location: z.string().optional(),
+})
+
+export const listJobsSchemaResponse = z.object({
+  jobs: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      description: z.string(),
+      company: z.string(),
+      location: z.string(),
+      type: z.enum(['REMOTE', 'ONSITE', 'HYBRID']),
+      level: z.enum(['JUNIOR', 'PLENO', 'SENIOR']),
+      technologies: z.array(z.string()),
+    }),
+  ),
+})
+
+export type ListJobsSchema = z.infer<typeof listJobsSchema>
+export type ListJobsSchemaResponse = z.infer<typeof listJobsSchemaResponse>
