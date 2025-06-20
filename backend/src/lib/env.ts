79f9073b31e9b0422e5070fbd8f2bc1dcff8282f
Gui-dev <adrianon10@gmail.com>
2025-06-20 04:11:49 -0300

    chore: add env test

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -4,6 +4,8 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
+  EMAIL_USER: z.string(),
+  EMAIL_PASS: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
