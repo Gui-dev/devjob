e5d18d71678c63a572c514191ce1e2cab3d43f89
Gui-dev <adrianon10@gmail.com>
2025-07-01 04:56:40 -0300

    test: it should be able to renders login page

---
+++ web/src/app/__tests__/login.spec.tsx
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import LoginPage from '../(auth)/login/page'
+import { AuthProvider } from '@/components/auth-provider'
+import { useRouter } from 'next/navigation'
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn(),
+}))
+
+vi.mock('next-auth/react', async () => {
+  const mod =
+    await vi.importActual<typeof import('next-auth/react')>('next-auth/react')
+  return {
+    ...mod,
+    signIn: vi.fn(),
+  }
+})
+
+describe('Login Page', () => {
+  const pushMock = vi.fn()
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+    vi.mocked(useRouter).mockReturnValue({
+      push: pushMock,
+    } as unknown as ReturnType<typeof useRouter>)
+  })
+
+  it('should be able to renders login page', () => {
+    render(
+      <AuthProvider>
+        <LoginPage />
+      </AuthProvider>,
+    )
+
+    expect(screen.getByText(/faça login/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/senha/i)).toBeInTheDocument()
+  })
+})
