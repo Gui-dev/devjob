d201962ab28adf62f1de78dd71bcce8a7c3acc58
Gui-dev <adrianon10@gmail.com>
2025-07-31 04:50:48 -0300

    feat: add use get job applications hook

---
+++ web/src/hooks/use-get-job-applications.ts
@@ -0,0 +1,44 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
+
+import { getJobApplications } from '@/services/get-job-applications'
+
+export type JobApplication = {
+  id: string
+  jobId: string
+  userId: string
+  message: string
+  githubUrl: string
+  linkedinUrl: string
+  status: 'PENDING' | 'ACCEPTED' | 'REJECTED'
+  createdAt: Date
+  job: {
+    id: string
+    recruiterId: string
+    title: string
+    description: string
+    company: string
+    location: string
+    type: 'REMOTE' | 'ONSITE' | 'HYBRID'
+    createdAt: Date
+  }
+}
+
+interface IGetJobApplicationsResponse {
+  jobApplications: JobApplication[]
+  meta: {
+    total: number
+    page: number
+    pages: number
+  }
+}
+
+export const useGetJobApplications = () => {
+  const { data } = useSuspenseQuery<IGetJobApplicationsResponse>({
+    queryKey: ['job-applications'],
+    queryFn: () => getJobApplications(),
+  })
+
+  return {
+    data,
+  }
+}
