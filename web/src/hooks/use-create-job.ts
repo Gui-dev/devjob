0017bead40df8cf1bd9aab9bd197e7def5ce496f
Gui-dev <adrianon10@gmail.com>
2025-08-19 05:15:26 -0300

    feat: add use create job hook

---
+++ web/src/hooks/use-create-job.ts
@@ -0,0 +1,23 @@
+import { api } from '@/lib/api'
+import type { CreateJobSchemaData } from '@/validations/create-job-schema'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+export const useCreateJob = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: CreateJobSchemaData) => {
+      const { data: result } = await api.post('/jobs', data)
+      return result
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['jobs'] })
+      queryClient.invalidateQueries({ queryKey: ['job-applications'] })
+    },
+  })
+
+  return {
+    mutate,
+    isPending,
+  }
+}
