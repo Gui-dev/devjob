b45f138e19b6a24db41922357cf0ff27d4aabf1d
Gui-dev <adrianon10@gmail.com>
2025-09-17 20:35:06 -0300

    feat: add skeleton job card component

---
+++ web/src/components/skeleton-job-card.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardHeader } from './ui/card'
+import { Skeleton } from './ui/skeleton'
+
+export const JobCardSkeleton = () => {
+  return (
+    <Card className="transition-all duration-200">
+      <CardHeader>
+        <Skeleton className="h-6 w-3/4 mb-2" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+      <CardContent className="space-y-3">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-[90%]" />
+        <Skeleton className="h-4 w-[80%]" />
+        <div className="flex gap-2">
+          <Skeleton className="h-6 w-20 rounded-full" />
+          <Skeleton className="h-6 w-16 rounded-full" />
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
